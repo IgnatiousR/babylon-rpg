5e369a85db9df218684fbb24a925daf7400ea7e3
Ignatious Rozario <ignatiousr80@gmail.com>
2026-02-23 22:41:05 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+import { main } from "./scenes/main";
 import "./style.css";
 import * as BABYLON from "@babylonjs/core";
-console.log(BABYLON);
+
+const canvas = document.querySelector("canvas");
+
+let engine = new BABYLON.Engine(canvas, true);
+let currentScene = new BABYLON.Scene(engine);
+
+const camera = new BABYLON.FreeCamera(
+  "camera1",
+  new BABYLON.Vector3(0, 0, 0),
+  currentScene,
+);
+
+await main(BABYLON, engine, currentScene);
