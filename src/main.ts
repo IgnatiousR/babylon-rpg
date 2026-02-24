35c4b4703930d277a7465494ea93a67d6efdfd3c
Ignatious <ignatiousr80@gmail.com>
2026-02-24 10:57:55 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ const canvas = document.querySelector("canvas");
 let engine = new BABYLON.Engine(canvas, true);
 let currentScene = new BABYLON.Scene(engine);
 
-const camera = new BABYLON.FreeCamera(
-  "camera1",
-  new BABYLON.Vector3(0, 0, 0),
-  currentScene,
-);
+const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, 0), currentScene);
 
-await main(BABYLON, engine, currentScene);
+await main(engine, currentScene);
