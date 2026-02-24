efdbdaea1171ab4a4cdfd03bdbd8425777ee232a
Ignatious <ignatiousr80@gmail.com>
2026-02-24 15:02:56 +0600

    Create index.ts

---
+++ src/input/index.ts
@@ -0,0 +1,6 @@
+export { Input, InputManager } from "./InputManager";
+export type { IInputSource } from "./IInputSource";
+export type { ActionName, AxisName } from "./types";
+export { VirtualJoystick } from "./mobile/VirtualJoystick";
+export { VirtualJoystickInput } from "./mobile/VirtualJoystickInput";
+export { TouchButton } from "./mobile/TouchButton";
